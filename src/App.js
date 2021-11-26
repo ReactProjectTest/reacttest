c33e022f782bb4914bc564695be305b2f9434ad0
Jeland Quinamot <quinamotjeland@gmail.com>
2021-11-26 17:36:30 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  Routes,
+  Route
+} from "react-router-dom";
+import About from "./pages/About";
+import Homepage from "./pages/Homepage";
+import Contact from "./pages/Contact";
+import React, {useState} from 'react';
+import Test from "./components/Test";
 
 function App() {
+  const [name, setName] = useState("");
+
+  const eventHandler = (e) => {
+    setName(e.target.value);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Test />
+    <Routes>
+        <Route path="/" element={<Homepage />}/>
+        <Route path="/contact" element={<Contact />}/>
+        <Route path="/about" element={<About />}/>
+    </Routes>
+    <input type="text" placeholder={name} onChange={eventHandler}/>
+    <p> {name} </p>
+    </>
   );
 }
 
